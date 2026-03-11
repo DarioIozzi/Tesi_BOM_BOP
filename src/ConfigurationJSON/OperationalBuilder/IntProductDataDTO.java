e6f17e001cbb3678eff95cb3f50cc16c64fb199f
Dario <dario.iozzi@edu.unifi.it>
2026-03-11 23:22:36 +0100

    Aggiunta IntProductDataDTO

---
+++ src/ConfigurationJSON/OperationalBuilder/IntProductDataDTO.java
@@ -0,0 +1,27 @@
+package ConfigurationJSON.OperationalBuilder;
+
+import Operational.Product;
+
+public class IntProductDataDTO {
+
+    private int quantity;
+    private Product product;
+
+    public IntProductDataDTO(){}
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
